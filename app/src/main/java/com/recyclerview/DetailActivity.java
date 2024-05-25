3020603ab653788b289f06bbebe2b8899258746f
Le Nguyen Tien Nhat <xlenhat1602x@gmail.com>
2024-05-25 17:08:29 +0700

    b

--- app/src/main/java/com/recyclerview/DetailActivity.java
+++ app/src/main/java/com/recyclerview/DetailActivity.java
@@ -1,10 +1,6 @@
 package com.recyclerview;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity {
